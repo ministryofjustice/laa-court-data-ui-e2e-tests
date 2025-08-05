9a7086ad8e225e1315f23e15ffac471fd9649c25
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2025-08-05 17:04:44 +0100

    Copy-paste from NSCC - sort out env vars later

---
+++ build_test_local.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Exit immediately if there is an error
+set -e
+export DOCKER_FILES="-f docker-compose.yml -f docker-compose.local.yml"
+
+if [[ $(uname -m) == 'arm64' ]];
+then
+  echo "Apple Silicon detected"
+  export DOCKER_BUILDKIT=1
+  export LOCAL_PLATFORM=linux/amd64
+else
+  export DOCKER_BUILDKIT=0
+fi
+
+function build {
+  docker-compose $DOCKER_FILES down --volumes --rmi "all"
+  docker-compose $DOCKER_FILES build --no-cache
+  docker-compose $DOCKER_FILES up -d
+}
+
+function shellin {
+  docker-compose run --entrypoint sh laa-court-data-end-to-end-tests
+}
+
+build
+shellin
+# now run `npx playwright test --trace on` in container
