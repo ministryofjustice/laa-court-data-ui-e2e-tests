70eaebeb5ccebf2561ca99426204ad0c6bb9b3a7
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2025-09-05 09:50:55 +0100

    Test hearing navigation links

---
+++ steps/hearing_detail_steps.js
@@ -0,0 +1,44 @@
+import { expect } from '@playwright/test'
+import { HearingDetailPage } from '../pages/hearing_detail_page';
+
+export const ORDERED_HEARING_DATES = [
+  '23/10/2019',
+  '24/10/2019',
+  '26/10/2019',
+  '27/10/2019',
+  '28/10/2019',
+  '29/10/2019',
+  '30/10/2019',
+  '31/10/2019',
+]
+
+export class HearingDetailSteps {
+  constructor(page) {
+    this.page = page
+    this.hearingDetailPage = new HearingDetailPage(page)
+  }
+
+  async andIClickNext() {
+    await this.hearingDetailPage.clickNext()
+  }
+
+  async andIClickPrevious() {
+    await this.hearingDetailPage.clickPrevious()
+  }
+
+  async thenIShouldSeeTheDetailsPageForTheFirstHearing() {
+    await this.hearingDetailPage.expectTitle(ORDERED_HEARING_DATES[0])
+  }
+
+  async thenIShouldSeeTheDetailsPageForTheSecondHearing() {
+    await this.hearingDetailPage.expectTitle(ORDERED_HEARING_DATES[1])
+  }
+
+  async thenIShouldSeeTheDetailsPageForTheLastHearing() {
+    await this.hearingDetailPage.expectTitle(ORDERED_HEARING_DATES[7])
+  }
+
+  async thenIShouldSeeTheDetailsPageForTheSecondLastHearing() {
+    await this.hearingDetailPage.expectTitle(ORDERED_HEARING_DATES[6])
+  }
+}
--- tests/case_hearings.spec.js
+++ tests/case_hearings.spec.js
@@ -1,20 +1,39 @@
 import { test } from '@playwright/test'
 import { SignInSteps } from '../steps/sign_in_steps'
 import { CaseDetailSteps } from '../steps/case_detail_steps'
+import { HearingDetailSteps } from '../steps/hearing_detail_steps'
 
 test.describe('Sort hearings', () => {
   let signInSteps
   let caseDetailSteps
+  let hearingDetailSteps
 
   test.beforeEach(async ({ page }) => {
     signInSteps = new SignInSteps(page)
     caseDetailSteps = new CaseDetailSteps(page)
+    hearingDetailSteps = new HearingDetailSteps(page)
   })
 
   test('Hearings are sorted by date by default', async () => {
     await signInSteps.givenIAmSignedInAsACaseworker();
     await caseDetailSteps.whenIVisitTheSummaryPageOfACase();
     await caseDetailSteps.thenHearingsShouldBeSortedByDateAscending();
+  })
+
+  test('Hearing details pages have forward links', async () => {
+    await signInSteps.givenIAmSignedInAsACaseworker();
+    await caseDetailSteps.whenIVisitTheSummaryPageOfACase();
+    await caseDetailSteps.andIClickOnTheFirstHearingDate();
+    await hearingDetailSteps.andIClickNext();
+    await hearingDetailSteps.thenIShouldSeeTheDetailsPageForTheSecondHearing();
+  })
+
+  test('Hearing details pages have backwards links', async () => {
+    await signInSteps.givenIAmSignedInAsACaseworker();
+    await caseDetailSteps.whenIVisitTheSummaryPageOfACase();
+    await caseDetailSteps.andIClickOnTheLastHearingDate();
+    await hearingDetailSteps.andIClickPrevious();
+    await hearingDetailSteps.thenIShouldSeeTheDetailsPageForTheSecondLastHearing();
   })
 
   test('Hearings date direction can be reversed', async () => {
@@ -34,7 +53,7 @@ test.describe('Sort hearings', () => {
   test('Hearings are clickable', async () => {
     await signInSteps.givenIAmSignedInAsACaseworker();
     await caseDetailSteps.whenIVisitTheSummaryPageOfACase();
-    await caseDetailSteps.andIClickOnAHearingDate();
-    await caseDetailSteps.thenIShouldSeeTheHearingDetailsPageForThatDate();
+    await caseDetailSteps.andIClickOnTheFirstHearingDate();
+    await hearingDetailSteps.thenIShouldSeeTheDetailsPageForTheFirstHearing();
   })
 })
